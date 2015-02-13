82226d0029140ba69cd8e0135a4207a58973033b
Novar-EE <Novar-EE@users.noreply.github.com>
2015-02-13 14:48:42 +0800

    1009 AC

---
+++ 1009/1009.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stdio.h>
+#include <stack>
+
+int main()
+{
+	
+//	char *tmp;
+//	scanf("%s\n", tmp);
+//	std::string in_str(tmp);
+	
+//	std::cin >> in_str;
+	std::stack<std::string> stack_str;
+	
+	while(true){
+		std::string str;
+		std::cin >> str;
+		stack_str.push(str);
+		
+		char c = getchar();
+		if (c == '\n')
+			break;		
+	}
+	
+	while(!stack_str.empty()){
+		if (stack_str.size() != 1)
+			std::cout << stack_str.top() << " "; 
+		else
+			std::cout << stack_str.top() << std::endl; 
+		stack_str.pop();
+	}
+	
+}
