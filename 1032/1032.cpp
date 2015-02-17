de4fbce50d282d20bdcfe51102951eeabace7ec1
Novar-EE <Novar-EE@users.noreply.github.com>
2015-02-17 23:31:02 +0800

    use a better way

--- 1032/1032.cpp
+++ 1032/1032.cpp
@@ -3,34 +3,24 @@
 #include <algorithm>
 
 int A[100010] = {0};
-int B[100010] = {0};
 
 int main()
 {
 	int N;	
 	scanf("%d", &N); getchar();
 	int num = 0;
+	int M = 0, M_index = 0;
 	
 	for (int i = 0; i < N; i ++){		
 		int tmp_id, tmp_score;	
 		scanf("%d %d", &tmp_id, &tmp_score); getchar();
 		A[tmp_id] += tmp_score;
 		
-		// 计算有效成绩的个数（编号是连续的） 
-		if (B[tmp_id] == 0){
-			num ++;
-			B[tmp_id] = 1;
+		if (A[tmp_id] > M){
+			M = A[tmp_id];
+			M_index = tmp_id;
 		}
 	}
-	
-	int M = 0, M_index = 0;
-	for (int i = 0; i <= num; i ++){
-		if (A[i] > M){
-			M = A[i];
-			M_index = i;	
-		}
-	}
-	
 	printf("%d %d\n", M_index, M);
 	
 }
