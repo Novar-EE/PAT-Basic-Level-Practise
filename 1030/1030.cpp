20ad94f43a7db23d7ce81154383eca23cd983059
Novar-EE <Novar-EE@users.noreply.github.com>
2015-02-17 21:35:58 +0800

    1030 20/25 time over as excepted....

---
+++ 1030/1030.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+#include <stdlib.h>
+
+long long A[100010] = {0};
+
+int main()
+{
+	int N;
+	long long p;
+	scanf("%d %lld\n", &N, &p);
+	for (int i = 0; i < N; i ++){
+		scanf("%d", &A[i]); getchar();
+	}	
+	
+	int len = 0;
+	int m = 0;
+	int M_index = 0;
+	
+	std::sort(A, A + N);
+	
+	for (int i = 0; i < N; i ++){
+		m = A[i];
+		double mp = 1.0 * m * p;
+		M_index = N - 1;
+		while (A[M_index] > m && A[M_index] > mp){
+			M_index --;
+		}
+		int tmp_len = M_index - i + 1;
+		if (len < tmp_len){
+			len = tmp_len;
+		}
+	}
+	
+	printf("%d\n", len);
+	
+	
+//	for (int i = 0; i < N; i ++){
+//		printf("%d ", A[i]);
+//	}
+
+}
